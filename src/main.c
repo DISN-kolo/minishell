450aeb72b4277824c8fe5e5db50d8cda7f005f9a
Artem Kozin <akozin@car7s1.42barcelona.com>
2024-03-19 16:00:53 +0100

    exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 12:06:29 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/19 15:56:37 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/19 16:00:30 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,11 @@ int	main(void)
 	s = readline("totally-not-bash $ ");
 	while (s != 0)
 	{
-		printf("%s\n", s);
 		add_history(s);
 		free(s);
 		s = readline("totally-not-bash $ ");
 	}
+	if (s == 0)
+		printf("exit\n");
 	return (0);
 }
