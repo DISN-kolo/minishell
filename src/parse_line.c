336b4c673faecf9e090da9bb1ba18879b5139975
Artem Kozin <akozin@car2s1.42barcelona.com>
2024-03-27 13:07:30 +0100

    small changes, starting a todo version of parseline

--- src/parse_line.c
+++ src/parse_line.c
@@ -6,15 +6,24 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 17:19:46 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/26 13:56:47 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/27 13:06:11 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+/*
+ * TODO
+ * make all of this sepped by funcs and norminette compliant n stuff
+ */
 void	parse_line(t_data *data)
 {
+	char	*onecom;
+
+	onecom = malloc(one_com_len_c(data, i)); //TODO int one_com_len_c(t_data *data, int first)
+	if (!onecom)
+		return ; //TODO erroring out
+	one_com_filler(data, i, &onecom); //TODO (int for error?) void one_com_filler(t_data *data, int i,)
 	if (data->errored)
 		return ;
-
 }
--- test.sh
+++ test.sh
@@ -3,3 +3,6 @@ c='echo | < > echo $v'
 $c
 c="echo | < > echo $v"
 $c
+v="as\"d "
+b="   q\"we"
+echo $v$b
