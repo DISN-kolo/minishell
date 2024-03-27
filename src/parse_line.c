271559d5d4962e155c5ca2f2744709434d0c4b38
Artem Kozin <akozin@car3s3.42barcelona.com>
2024-03-27 14:18:46 +0100

    proto parse

--- src/parse_line.c
+++ src/parse_line.c
@@ -6,24 +6,39 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 17:19:46 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/27 13:53:26 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/27 14:17:02 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
+int	pass_exp(t_data *data)
+{
+	int	i;
+
+	i = -1;
+	while (data->tokens[i++])
+		printf("%s ", data->tokens[i]);
+	printf("\n");
+	return (0);
+}
+
 /*
- * TODO
- * make all of this sepped by funcs and norminette compliant n stuff
+ * pass 1:
+ * check for exports.
+ * with each export, set the new env
+ *
+ * pass 2:
+ * grab commands. expand $. when an export command is encountered (without
+ * a | in the end), incriment the index of env used by 1.
+ * each grabbed command is ran with its parameters. the exit status is given.
  */
 void	parse_exec(t_data *data)
 {
-	char	*com;
-
-	com = malloc(com_len_c(data, i)); //TODO int one_com_len_c(t_data *data, int first)
-	if (!com)
-		return ; //TODO erroring out
-	com_filler(data, i, &com); //TODO (int for error?) void one_com_filler(t_data *data, int i,)
+	if (pass_exp(data)) // TODO
+		return ; // TODO exit with export checking error
+//	if (pass_com(data)) // TODO
+//		return ; // TODO exit with a meta exec error or with a parse error
 	if (data->errored)
 		return ;
 }
