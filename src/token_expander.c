46ad2fd8df75ff7f852ba22f8a25f9556ad744b5
Artem Kozin <akozin@car5s4.42barcelona.com>
2024-03-28 12:33:55 +0100

    rework and more work on token expansion. now i have to fill the token, and do an ft_split with quotations in mind

--- src/token_expander.c
+++ src/token_expander.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 16:41:25 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/27 18:08:48 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/28 12:29:48 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ int	ins_val_c_internal(char *s, char *sep, int *in_q, int i)
 	count = 0;
 	while (s[i])
 	{
-		if (*in_q == 1 && (s[i] == '\'' || s[i] == '"'))
+		if ((*in_q == 1 && s[i] == '\'') || (*in_q == 2 && s[i] == '"'))
 			*in_q = 0;
 		else if (!*in_q && (s[i] == '\'' || s[i] == '"'))
-			*in_q = 1;
+			*in_q = (s[i] == '"') + 1;
 		if ((!*in_q && !ft_strchr(sep, s[i]) && s[i] != '\''
 				&& s[i] != '"') || (*in_q == 1 && s[i] == '\'')
 			|| (*in_q == 2 && s[i] == '"'))
@@ -54,7 +54,7 @@ int	inside_val_counter(char *s, char *sep)
 	{
 		i++;
 		count++;
-		in_q = 1;
+		in_q = 1 + (s[i - 1] == '"');
 	}
 	else if (!ft_strchr(sep, s[i++]))
 		count++;
@@ -65,49 +65,53 @@ int	inside_val_counter(char *s, char *sep)
 int	inside_dollar_counter(t_data *data, char *t, int i)
 {
 	int		ret;
-	int		start;
 	char	*env_v_name;
 	char	*env_v_val;
 
-	start = i + 1;
-	if (!strchars(&(t[start]), " \t\f\v!@#$%^&*()-=+[]{}\\;:,./?"))
-		env_v_name = ft_substr(&(t[start]), 0, ft_strlen(&(t[start])));
-	else
-		env_v_name = ft_substr(&(t[start]), 0,
-				strchars(&(t[start]), " \t\f\v!@#$%^&*()-=+[]{}\\;:,./?")
-				- &(t[start]));
+	if (t[i + 1] != '_' && !ft_isalpha(t[i + 1]))
+		return (0);
+	env_v_name = ft_substr(&t[i + 1], 0, var_end(&t[i + 1]) - &t[i + 1]);
 	env_v_val = get_env_var_by_name(data, env_v_name); // TODO placeholder. gets the value by key, accounting for envi
-	return (inside_val_counter(env_v_val, " \t\f\v"));
+	ret = ft_strlen(env_v_val) - ft_strlen(env_v_name);
+	free(env_v_name);
+	free(env_v_val);
+	return (ret);
 }
 
 int	expansion_counter(t_data *data, char *t)
 {
 	int	ret;
+	int	temp_c;
 	int	i;
-	int	j;
 	int	in_q;
 
-	ret = 1;
+	ret = 0;
 	i = 0;
-	j = 0;
 	in_q = 0;
 	while (t[i])
 	{
-		if (in_q && (t[i] == '\'' || t[i] == '"'))
-			in_q = 0;
-		else if (!in_q && (t[i] == '\'' || t[i] == '"'))
-			in_q = 1;
-		if (!in_q && t[i] == '$')
-			ret += inside_dollar_counter(data, t, i);
+		if ((*in_q == 1 && t[i] == '\'') || (*in_q == 2 && t[i] == '"'))
+			*in_q = 0;
+		else if (!*in_q && (t[i] == '\'' || t[i] == '"'))
+			*in_q = (t[i] == '"') + 1;
+		if (in_q != 1 && t[i] == '$')
+		{
+			ret += inside_dollar_counter(data, t, i) - 1;
+		}
 		i++;
 	}
+	return (i + ret);
 }
 
 char **token_expander(t_data *data, char *t)
 {
-	char	**add_argv;
+	char	**new_tokens;
+	char	*expanded_token;
 
-	add_argv = malloc(sizeof (char *) * (expansion_counter(data, t) + 1));
+	expanded_token = malloc(expansion_counter(data, t));
+	if !(expanded_token)
+		return (NULL); // TODO ?
+	new_tokens = malloc(sizeof (char *) * (expansion_counter(data, t) + 1));
 	if !(add_argv)
 		return (NULL); // TODO ?
 }
--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@ c='echo | < > echo $v'
 $c
 c="echo | < > echo $v"
 $c
-v="as\"d "
-b="   q\"we"
-echo $v$b
+v="as\"d        "
+b="        q\"we"
+ls "i$v$b"
 q="asd\nqwe"
 echo $q
 ls $q
