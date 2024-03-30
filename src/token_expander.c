734d3ce320f0ddf32aef2631a96aecac1c73a251
Artem Kozin <akozin@car6s2.42barcelona.com>
2024-03-30 14:38:15 +0100

    midpoint

--- src/token_expander.c
+++ src/token_expander.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 16:41:25 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/28 16:54:40 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/30 14:36:04 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
  * LITERALLY tokenizer count logic, but without the <>|& stuff
  * lol it's actually quite a lot more simple
  */
-static void	fill_var(char **f_me, char *t, t_data *data, int *j)
+static void	fill_var(t_token *f_me, char *t, t_data *data, int *j)
 {
 	char	*env_v_name;
 	char	*env_v_val;
@@ -40,7 +40,7 @@ static void	fill_var(char **f_me, char *t, t_data *data, int *j)
 /* ^^^ being re-made
  */
 
-static int	token_expander(char *f_me, chart_data *data, char *t)
+static int	dollar_expander(t_token *f_me, t_data *data, int envi, char *t)
 {
 	int		i;
 	int		j;
@@ -62,7 +62,12 @@ static int	token_expander(char *f_me, chart_data *data, char *t)
 			i += var_end(&t[i + 1]) - &t[i + 1];
 		}
 		else
-			f_me[j] = t[i];
+			f_me.token[j] = t[i];
+		if ((*in_q == 1 && t[i] == '\'') || (*in_q == 2 && t[i] == '"')
+				|| (!*in_q && ft_strchr(" \t\f\v'\"", t[i])))
+			f_me.literal[j] = 0;
+		else
+			f_me.literal[j] = 1;
 		i++;
 		j++;
 	}
@@ -71,7 +76,7 @@ static int	token_expander(char *f_me, chart_data *data, char *t)
 /* ^^^ being re-made
  */
 
-static int	inside_dollar_counter(t_data *data, char *t, int i)
+static int	inside_dollar_counter(t_data *data, int envi, char *t, int i)
 {
 	int		ret;
 	char	*env_v_name;
@@ -80,14 +85,14 @@ static int	inside_dollar_counter(t_data *data, char *t, int i)
 	if (t[i + 1] != '_' && !ft_isalpha(t[i + 1]))
 		return (0);
 	env_v_name = ft_substr(&t[i + 1], 0, var_end(&t[i + 1]) - &t[i + 1]);
-	env_v_val = read_env(data, env_v_name);
+	env_v_val = read_env(data, env_v_name); // TODO envi? madonn
 	ret = ft_strlen(env_v_val) - ft_strlen(env_v_name);
 	free(env_v_name);
 	free(env_v_val);
 	return (ret);
 }
 
-static int	expansion_counter(t_data *data, char *t)
+static int	expansion_counter(t_data *data, int envi, char *t)
 {
 	int	ret;
 	int	i;
@@ -103,34 +108,31 @@ static int	expansion_counter(t_data *data, char *t)
 		else if (!*in_q && (t[i] == '\'' || t[i] == '"'))
 			*in_q = (t[i] == '"') + 1;
 		if (in_q != 1 && t[i] == '$')
-			ret += inside_dollar_counter(data, t, i) - 1;
+			ret += inside_dollar_counter(data, envi, t, i) - 1;
 		i++;
 	}
 	return (i + ret);
 }
 
-t_token *token_expander(t_data *data, char *t, int envi)
+t_token *token_expander(t_data *data, int envi, char *t)
 {
 	int		i;
 	t_token	*new_tokens;
 	t_token	*local_n_t;
-	char	*exp_t;
-	int		*exp_l;
+	t_token	exp_t;
 
 	i = 0;
 	while (data->tokens[i].token) // TODO norm, returns
 	{
-		exp_t = malloc(expansion_counter(data, envi, t));
-		if !(expanded_token)
+		exp_t.token = malloc(expansion_counter(data, envi, data->tokens[i].token));
+		exp_t.literal = malloc(sizeof (int) * expansion_counter(data, envi, data->tokens[i].token));
+		if (!exp_t.token || !exp_t.literal)
 			return (NULL);
-		exp_l = malloc(sizeof (int) * expansion_counter(data, envi, t));
-		if !(expanded_literals)
-			return (NULL);
-		token_expander(exp_t, exp_l, data, envi);
-		local_n_t = malloc(sizeof (t_token) * (new_t_c(exp_t, exp_l) + 1));
+		dollar_expander(&exp_t, data, envi);
+		local_n_t = malloc(sizeof (t_token) * (new_t_c(exp_t) + 1));
 		if (!local_n_t)
 			return (NULL);
-		local_n_t = new_t_split(exp_t, exp_l);
+		local_n_t = new_t_split(exp_t);
 		new_tokens = tokens_join_free(new_tokens, local_n_t);
 		if (!new_tokens)
 			return (NULL);
--- src/token_expander_utils.c
+++ src/token_expander_utils.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:01:48 by akozin            #+#    #+#             */
-/*   Updated: 2024/03/28 13:37:40 by akozin           ###   ########.fr       */
+/*   Updated: 2024/03/30 13:26:16 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,44 @@ char	*var_end(char *s)
 		s++;
 	}
 	return (s);
+}
+
+t_token	*tokens_join(t_token *t1, t_token *t2) // TODO literal int array copy?
+{
+	t_token	*ret;
+	int		i;
+	int		j;
+
+	ret = malloc(tokenslen(t1) + tokenslen(s2) + 1);
+	if (!ret)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (t1[j].token)
+	{
+		ret[i].token = ft_substr(t1[j].token, 0, ft_strlen(t1[j].token));
+		i++;
+		j++;
+	}
+	j = 0;
+	while (t2[j].token)
+	{
+		ret[i].token = ft_substr(t2[j].token, 0, ft_strlen(t2[j].token));
+		i++;
+		j++;
+	}
+	ret[i].token = 0;
+	return (ret);
+}
+
+t_token	*tokens_join_free(t_token *t1, t_token *t2)
+{
+	t_token	*ret;
+
+	ret = tokens_join(t1, t2);
+	if (t1)
+		free_ret(&t1);
+	return (ret);
 }
 
 /*
