9f64417eed52250f41c013da04b9fe29e69e6868
Molasz <martiolasz@gmail.com>
2024-04-10 11:42:57 +0200

    fixes & normi

--- src/token_loop.c
+++ src/token_loop.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/03 14:59:19 by akozin            #+#    #+#             */
-/*   Updated: 2024/04/09 15:40:00 by akozin           ###   ########.fr       */
+/*   Updated: 2024/04/10 11:42:04 by molasz-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	cmd_count(t_token *token)
 	count = 1;
 	j = 0;
 	while (token[j].token && ft_strncmp(token[j].token, "||", 3)
-			&& ft_strncmp(token[j].token, "&&", 3))
+		&& ft_strncmp(token[j].token, "&&", 3))
 	{
 		if (!ft_strncmp(token[j].token, "|", 2))
 			count++;
@@ -42,27 +42,26 @@ static int	cmd_len(t_token *token)
 static int	cmd_loop(t_data *data, t_token *token, int *count)
 {
 	int	cmd_l;
-	int	j;
-	int	k;
-	int	l;
+	int	i[3];
 
 	cmd_l = cmd_count(token);
 	data->coms = malloc((cmd_l + 1) * sizeof (t_com));
 	if (!data->coms)
 		return (1);
-	j = -1;
-	while (++j < cmd_l)
+	data->coms[cmd_l].com = NULL;
+	i[0] = -1;
+	while (++i[0] < cmd_l)
 	{
-		k = cmd_len(data->tokens + *count);
-		data->coms[j].com = malloc((k + 1) * sizeof (char *));
-		if (!data->coms[j].com)
+		i[1] = cmd_len(data->tokens + *count);
+		data->coms[i[0]].com = malloc((i[1] + 1) * sizeof (char *));
+		if (!data->coms[i[0]].com)
 			return (free_coms(data), 1);
-		data->coms[j].com[k] = NULL;
-		l = -1;
-		while (++l < k)
-			data->coms[j].com[l] = data->tokens[*count + l].token;
-		*count += l;
-		if (j + 1 < cmd_l)
+		data->coms[i[0]].com[i[1]] = NULL;
+		i[2] = -1;
+		while (++i[2] < i[1])
+			data->coms[i[0]].com[i[2]] = data->tokens[*count + i[2]].token;
+		*count += i[2];
+		if (i[0] + 1 < cmd_l)
 			*count += 1;
 	}
 	return (0);
@@ -75,7 +74,8 @@ int	token_loop(t_data *data)
 	int	count;
 
 	cmd_c = 1;
-	while (data->tokens[cmd_c].token && (!ft_strncmp(data->tokens[cmd_c].token, "||", 3)
+	while (data->tokens[cmd_c].token
+		&& (!ft_strncmp(data->tokens[cmd_c].token, "||", 3)
 			|| !ft_strncmp(data->tokens[cmd_c].token, "&&", 3)))
 		cmd_c++;
 	i = 0;
