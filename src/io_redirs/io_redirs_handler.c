2a6b50f3d3d4e3c1222663838a0c82a418469990
Artem Kozin <akozin@car10s2.42barcelona.com>
2024-04-23 17:01:30 +0200

    io changes to structs complete, NOT bugtested

--- src/io_redirs/io_redirs_handler.c
+++ src/io_redirs/io_redirs_handler.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 13:58:31 by akozin            #+#    #+#             */
-/*   Updated: 2024/04/23 16:40:54 by akozin           ###   ########.fr       */
+/*   Updated: 2024/04/23 17:01:05 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@
 int	open_everything(t_data *data)
 {
 	int	i[3];
-	int	fd;
+	int	fi;
+	int	fo;
 	int	flags;
 
 	i[0] = 0;
@@ -28,34 +29,41 @@ int	open_everything(t_data *data)
 		data->coms[i[0]].infd = 0;
 		data->coms[i[0]].outfd = 1;
 		i[1] = 0;
-		while (data->coms[i[0]].ins[i[1]].fname)
+		fi = -42;
+		fo = -42;
+		while (data->coms[i[0]].ios[i[1]].fname)
 		{
-			if (data->coms[i[0]].ins[i[1]].dub
-					&& !data->coms[i[0]].ins[i[1] + 1].fname)
-				fd = heredoc_read_expand(i, data); // TODO
-			else if (!data->coms[i[0]].ins[i[1]].dub)
-				fd = open(data->coms[i[0]].ins[i[1]].fname, O_RDONLY);
-			if (fd == -1)
-				return (-2); // TODO non-fatal error. move on to the next command
-			else if (fd == -2)
-				return (-1); // TODO fatal error(?). someone screwed our heredoc tmp file!
-			if (data->coms[i[0]].ins[i[1] + 1].fname)
-				close(fd);
-			i[1]++;
-		}
-		i[1] = 0;
-		while (data->coms[i[0]].outs[i[1]].fname)
-		{
-			if (data->coms[i[0]].outs[i[1]].dub)
-				flags = O_WRONLY | O_CREAT | O_APPEND;
+			if (data->coms[i[0]].ios[i[1]].in)
+			{
+				if (fi != -42)
+					close(fi);
+				if (data->coms[i[0]].ios[i[1]].dub
+						&& !data->coms[i[0]].ios[i[1] + 1].fname)
+					fi = heredoc_read_expand(i, data); // TODO
+				else if (!data->coms[i[0]].ios[i[1]].dub)
+					fi = open(data->coms[i[0]].ios[i[1]].fname, O_RDONLY);
+				if (fi == -1)
+					return (-2); // TODO non-fatal error. move on to the next command
+				else if (fi == -2)
+					return (-1); // TODO fatal error(?). someone screwed our heredoc tmp file!
+			}
 			else
-				flags = O_WRONLY | O_CREAT | O_TRUNC;
-			fd = open(data->coms[i[0]].outs[i[1]].fname, flags);
-			if (data->coms[i[0]].outs[i[1] + 1].fname)
-				close(fd);
+			{
+				if (fo != -42)
+					close(fo);
+				if (data->coms[i[0]].ios[i[1]].dub)
+					flags = O_WRONLY | O_CREAT | O_APPEND;
+				else
+					flags = O_WRONLY | O_CREAT | O_TRUNC;
+				fo = open(data->coms[i[0]].ios[i[1]].fname, flags);
+				if (fo == -1)
+					return (-2); // TODO non-fatal error. move on to the next command
+			}
 			i[1]++;
+
 		}
-		data->coms[i[0]].outfd = fd;
+		data->coms[i[0]].infd = fi;
+		data->coms[i[0]].outfd = fo;
 		i[0]++;
 	}
 }
