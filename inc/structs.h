f13f43acd4aa131d462b59f6e2e451a405ba8801
molasz <martiolasz@gmail.com>
2024-04-24 19:39:26 +0200

    normi structs header file

--- inc/structs.h
+++ inc/structs.h
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   structs.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: molasz-a <molasz-a@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 19:37:44 by molasz-a          #+#    #+#             */
+/*   Updated: 2024/04/24 19:38:50 by molasz-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef STRUCTS_H
-#define STRUCTS_H
+# define STRUCTS_H
 
 /*
  * operators are &&, ||, |
@@ -20,7 +32,7 @@ typedef struct s_inout
 {
 	int		dub;
 	char	*fname;
-} t_inout;
+}	t_inout;
 
 typedef struct s_com
 {
@@ -28,28 +40,28 @@ typedef struct s_com
 	t_inout		*ins;
 	t_inout		*outs;
 	int			status;
-} t_com;
+}	t_com;
 
 typedef struct s_token
 {
 	char	*token;
 	int		*literal;
 	t_tok_s	type;
-} t_token;
+}	t_token;
 
 typedef struct s_env
 {
 	char	*key;
 	char	*value;
 	int		exp;
-} t_env;
+}	t_env;
 
 /*
  * expand? yeah -> we expand $ inside
  * nah -> we don't
  * determined by whether the hdoc str is in quotations or no (wow)
  */
-typedef struct	s_hdoc
+typedef struct s_hdoc
 {
 	char	*str;
 	int		expand;
@@ -64,6 +76,6 @@ typedef struct s_data
 	int		status_code;
 	int		errored;
 	t_env	*env;
-} t_data;
+}	t_data;
 
 #endif
