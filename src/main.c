d2d1d4fd49f28fb604cf87034618a661d8a5d209
Artem Kozin <akozin@car3s1.42barcelona.com>
2024-05-02 13:48:42 +0200

    signaling starting

--- src/main.c
+++ src/main.c
@@ -6,16 +6,43 @@
 /*   By: akozin <akozin@student.42barcelona.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 12:06:29 by akozin            #+#    #+#             */
-/*   Updated: 2024/04/05 14:37:43 by molasz-a         ###   ########.fr       */
+/*   Updated: 2024/05/02 13:47:22 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+#include "../libs/readline/readline.h"
+#include "../libs/readline/history.h"
+#include <signal.h>
 
+void	handle(int sig)
+{
+	//write(2, "sig: ", 5);
+	//write(2, sig == SIGINT ? "int " : sig == SIGQUIT ? "quit" : "tstp", 4);
+	//write(2, "\n", 1);
+	//printf("%d\n", sig);
+	(void)sig;
+	printf("\n");
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}
+
+/*
+ * ctrl-c = sigint
+ * ctrl-\ = sigquit
+ * ctrl-z = sigstop
+ */
 int	main(int argc, char **argv, char **envp)
 {
-	t_data	data;
+	t_data				data;
+	struct sigaction	s_sa;
 
+	s_sa.sa_handler = &handle;
+	s_sa.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &s_sa, NULL);
+	sigaction(SIGQUIT, &s_sa, NULL);
+	sigaction(SIGTSTP, &s_sa, NULL);
 	(void) argc;
 	(void) argv;
 	data.tokens = 0;
