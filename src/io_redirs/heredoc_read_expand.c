b547208f56e614ca827913505179182626d35f7c
Artem Kozin <akozin@car3s1.42barcelona.com>
2024-05-13 16:38:00 +0200

    funcionality fix using pointers to save the changes to the fname

--- src/io_redirs/heredoc_read_expand.c
+++ src/io_redirs/heredoc_read_expand.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 12:33:56 by akozin            #+#    #+#             */
-/*   Updated: 2024/05/13 16:24:51 by akozin           ###   ########.fr       */
+/*   Updated: 2024/05/13 16:33:23 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,13 +42,13 @@ void	hd_coords(int *hdi, int n, t_data *data)
 	}
 }
 
-static int	gnl_expand(t_data *data, char *fname, int hdfd)
+static int	gnl_expand(t_data *data, char **fname, int hdfd)
 {
 	int		susfd;
 	char	*curline;
 
-	fname = ft_strjoin_free(fname, "_exp");
-	susfd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	*fname = ft_strjoin_free(*fname, "_exp");
+	susfd = open(*fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (susfd == -1)
 		return (print_perror("Open failed in hdoc sust\n", -1), -2); // TODO think twice
 	curline = get_next_line(hdfd);
@@ -63,7 +63,6 @@ static int	gnl_expand(t_data *data, char *fname, int hdfd)
 	if (curline)
 		free(curline);
 	close(susfd);
-	printf("%s <- fname after all the good stuff and before exiting\n", fname);
 	return (0);
 }
 
@@ -80,10 +79,9 @@ int	heredoc_read_expand(t_data *data)
 		return (print_perror("Open failed in hdoc\n", -1), -2); // TODO think twice abt the err code
 	if (data->hds[hdi[0]][hdi[1]].expand)
 	{
-		if (gnl_expand(data, fname, hdfd) == -2)
+		if (gnl_expand(data, &fname, hdfd) == -2)
 			return (-2);
 		close(hdfd);
-		printf("%s <- fname after all the good stuff\n", fname);
 		hdfd = open(fname, O_RDONLY);
 		if (hdfd == -1)
 			return (print_perror("Second open failed in hdoc\n", -1), -2); // TODO think twice
