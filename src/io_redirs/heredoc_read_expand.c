0a3b1601e9a600d20a82f7c468ef5f2022c6e191
Artem Kozin <akozin@car3s1.42barcelona.com>
2024-05-13 16:25:20 +0200

    in medium changes

--- src/io_redirs/heredoc_read_expand.c
+++ src/io_redirs/heredoc_read_expand.c
@@ -6,7 +6,7 @@
 /*   By: akozin <akozin@student.42barcelon>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 12:33:56 by akozin            #+#    #+#             */
-/*   Updated: 2024/05/13 16:05:52 by akozin           ###   ########.fr       */
+/*   Updated: 2024/05/13 16:24:51 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,35 @@ void	hd_coords(int *hdi, int n, t_data *data)
 	}
 }
 
+static int	gnl_expand(t_data *data, char *fname, int hdfd)
+{
+	int		susfd;
+	char	*curline;
+
+	fname = ft_strjoin_free(fname, "_exp");
+	susfd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (susfd == -1)
+		return (print_perror("Open failed in hdoc sust\n", -1), -2); // TODO think twice
+	curline = get_next_line(hdfd);
+	while (curline)
+	{
+		curline = hd_dollar_expander(curline, data);
+		if (write(susfd, curline, ft_strlen(curline)) == -1)
+			return (print_perror("Write failed in hdoc sust\n", -1), -2); // TODO think twice
+		free(curline);
+		curline = get_next_line(hdfd);
+	}
+	if (curline)
+		free(curline);
+	close(susfd);
+	printf("%s <- fname after all the good stuff and before exiting\n", fname);
+	return (0);
+}
+
 int	heredoc_read_expand(t_data *data)
 {
 	char	*fname;
-	char	*curline;
 	int		hdfd;
-	int		susfd;
 	int		hdi[2];
 
 	hd_coords(hdi, data->hd_counter, data);
@@ -57,28 +80,14 @@ int	heredoc_read_expand(t_data *data)
 		return (print_perror("Open failed in hdoc\n", -1), -2); // TODO think twice abt the err code
 	if (data->hds[hdi[0]][hdi[1]].expand)
 	{
-		fname = ft_strjoin_free(fname, "_exp");
-		susfd = open(fname, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		if (susfd == -1)
-			return (print_perror("Open failed in hdoc sust\n", -1), -2); // TODO think twice
-		curline = get_next_line(hdfd);
-		while (curline)
-		{
-			curline = hd_dollar_expander(curline, data);
-			if (write(susfd, curline, ft_strlen(curline)) == -1)
-				return (print_perror("Write failed in hdoc sust\n", -1), -2); // TODO think twice
-			free(curline);
-			curline = get_next_line(hdfd);
-		}
-		if (curline)
-			free(curline);
+		if (gnl_expand(data, fname, hdfd) == -2)
+			return (-2);
+		close(hdfd);
+		printf("%s <- fname after all the good stuff\n", fname);
+		hdfd = open(fname, O_RDONLY);
+		if (hdfd == -1)
+			return (print_perror("Second open failed in hdoc\n", -1), -2); // TODO think twice
 	}
-	close(hdfd);
-	if (data->hds[hdi[0]][hdi[1]].expand)
-		close(susfd);
-	hdfd = open(fname, O_RDONLY);
-	if (hdfd == -1)
-		return (print_perror("Open failed in hdoc\n", -1), -2); // TODO think twice
 	free(fname);
 	return (hdfd);
 }
