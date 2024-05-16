8b98d8793f62fa9791daffbff2e947f7ece7efdd
Artem Kozin <akozin@car4s2.42barcelona.com>
2024-05-16 18:06:43 +0200

    exit codes in exec

--- src/runner/find_cmd.c
+++ src/runner/find_cmd.c
@@ -6,7 +6,7 @@
 /*   By: molasz-a <molasz-a@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 12:23:40 by molasz-a          #+#    #+#             */
-/*   Updated: 2024/05/16 16:47:29 by akozin           ###   ########.fr       */
+/*   Updated: 2024/05/16 17:55:36 by akozin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,28 +53,37 @@ static int	run_cmd(t_data *data, int i, char **env)
 			free_double(splited_path);
 	}
 	print_error(NULL, data->coms[i].com[0], "command not found");
-	return (1);
+	return (127);
 }
 
 static int	run_absolute(t_data *data, int i, char **env)
 {
+	int	err;
+
+	err = 1;
 	if (!access(data->coms[i].com[0], F_OK))
 	{
 		if (access(data->coms[i].com[0], X_OK))
 			print_error(NULL, data->coms[i].com[0], "Permission denied");
 		else if (execve(data->coms[i].com[0], data->coms[i].com, env))
 			print_error(NULL, data->coms[i].com[0], "is a directory");
+		err = 126;
 	}
 	else
+	{
 		print_error(NULL, data->coms[i].com[0], "No such file or directory");
-	return (1);
+		err = 127;
+	}
+	return (err);
 }
 
 static int	run_relative(t_data *data, int i, char **env)
 {
 	char	*pwd;
 	char	*path;
+	int		err;
 
+	err = 1;
 	pwd = getcwd(NULL, 0);
 	path = ft_strjoin3(pwd, "/", data->coms[i].com[0]);
 	if (!path)
@@ -85,12 +94,16 @@ static int	run_relative(t_data *data, int i, char **env)
 			print_error(NULL, data->coms[i].com[0], "Permission denied");
 		else if (execve(path, data->coms[i].com, env))
 			print_error(NULL, data->coms[i].com[0], "is a directory");
+		err = 126;
 	}
 	else
+	{
 		print_error(NULL, data->coms[i].com[0], "No such file or directory");
+		err = 127;
+	}
 	free(path);
 	free(pwd);
-	return (1);
+	return (err);
 }
 
 void	find_cmd(t_data *data, int i)
@@ -109,5 +122,5 @@ void	find_cmd(t_data *data, int i)
 		err = run_cmd(data, i, env);
 	free_double(env);
 	if (err)
-		exit (1);
+		exit(err);
 }
